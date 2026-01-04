525be848cb529a8f1ffe7af4c25bdfbac7946ea1
NIRAN-MUT <6811860013@MUT.AC.TH>
2026-01-04 17:22:42 +0700

    lab 7 and 8

---
+++ LAB7/TEST 0701/TEST 0701.cpp
@@ -0,0 +1,14 @@
+﻿// TODO: เขียนโปรแกรมแสดงค่า x และ *p
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int* p = &x;
+	cout << "Value of x: \t" << x << endl;
+	cout << "Value of *p: \t" << *p << endl;
+
+    // TODO
+
+    return 0;
+}
